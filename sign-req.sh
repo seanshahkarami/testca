8270b7c16f56982b2118293fcaf31067d9ff1f59
Sean Shahkarami <sean.shahkarami@gmail.com>
2017-07-27 10:16:09 -0500

    removed server extension from sign

--- sign-req.sh
+++ sign-req.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-# openssl ca -config openssl.cnf -in reqs/$1.pem -out certs/$1.pem -notext -batch -extensions server_ca_extensions
-openssl ca -config openssl.cnf -in reqs/$1.pem -out certs/$1.pem -notext -batch -extensions server_ca_extensions
+openssl ca -config openssl.cnf -in reqs/$1.pem -out certs/$1.pem -notext
