65bd94af68271afb88123c50977a25192d26a2e8
Sean Shahkarami <sean.shahkarami@gmail.com>
2017-08-22 15:50:29 -0500

    added name field

--- gen-req.sh
+++ gen-req.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 name=$1
-openssl req -new -newkey rsa:2048 -keyout private/$name.pem -nodes -out reqs/$1.pem -outform PEM -subj "/CN=$name/"
+openssl req -new -newkey rsa:2048 -keyout private/$name.pem -nodes -out reqs/$name.pem -outform PEM -subj "/CN=$name/"
